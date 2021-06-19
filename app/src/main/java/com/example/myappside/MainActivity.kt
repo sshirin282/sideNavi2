6530ee3ed385e46c71bdf5f19eb6db9c6a731cab
Azhar Shaikh <landmark.icommune@gmail.com>
2021-06-19 18:26:58 +0530

    home

---
+++ app/src/main/java/com/example/myappside/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myappside
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView=findViewById(R.id.navigation)
+
+
+        val fragment = FirstFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
+        true
+    }
+}
